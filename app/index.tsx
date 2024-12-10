4aa369d7d8bd21e3b54708064a8b0b8069febca7
milena579 <mileclaegari@gmail.com>
2024-12-10 14:07:46 -0300

    updates

--- app/index.tsx
+++ app/index.tsx
@@ -60,6 +60,7 @@ const styles =  StyleSheet.create({
         paddingHorizontal: 10,
         paddingVertical: 8,
         borderColor: "#ffffff",
+        borderWidth: 2,
         color: "#ffffff",
         width: "80%",
         borderRadius: 10
