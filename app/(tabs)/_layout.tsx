e3c64ff6d4416d116f895607ba6a3a5a892130d6
juupurcino <jdiaspurcino@gmail.com>
2024-12-16 16:58:33 -0300

    carrinho ok

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,16 +6,9 @@ export default function TabLayout() {
   return (
     <>
       <Tabs>
-<<<<<<< HEAD
-        <Tabs.Screen name="index" options={{headerShown: false, tabBarIcon: () => (<Text>😍</Text>)}}></Tabs.Screen>
-        <Tabs.Screen name="explore" options={{headerShown: false, tabBarIcon: () => (<Text>🤦‍♂️</Text>)}}></Tabs.Screen>
-        <Tabs.Screen name="carrinho" options={{headerShown: false, tabBarIcon: () => (<Text>🤦‍♂️</Text>)}}></Tabs.Screen>
-        <Tabs.Screen name="product" options={{headerShown: false, tabBarIcon: () => (<Text></Text>)}}></Tabs.Screen>
-=======
         <Tabs.Screen name="product" options={{headerShown: false, tabBarIcon: () => (<Image source={require("../../assets/images/user-alt-1-svgrepo-com 1.png")} />), tabBarLabel: () => null,}}></Tabs.Screen>
         <Tabs.Screen name="index" options={{headerShown: false, tabBarIcon: () => (<Image source={require("../../assets/images/home-4-svgrepo-com 1.png")} />), tabBarLabel: () => null,}}></Tabs.Screen>
         <Tabs.Screen  name="carrinho" options={{headerShown: false, tabBarIcon: () => ( <Image source={require("../../assets/images/buy-svgrepo-com (3) 1.png")} />), tabBarLabel: () => null,}}></Tabs.Screen>
->>>>>>> 94c0148c7378e666a73da804a5bb178f590c06f3
       </Tabs>
 
     </>
